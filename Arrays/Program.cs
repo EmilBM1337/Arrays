35accd9a0c06dfb4435b48080919a5a48e0eb28d
emil41m5 <emil41m5@CV-BB-5340>
2021-08-25 14:39:26 +0200

    Add project files.

---
+++ Arrays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mitArray = new int[10];
+
+            Random rnd = new Random();
+            mitArray[0] = rnd.Next(1001);
+            mitArray[1] = rnd.Next(1001);
+            mitArray[2] = rnd.Next(1001);
+            mitArray[3] = rnd.Next(1001);
+            mitArray[4] = rnd.Next(1001);
+            mitArray[5] = rnd.Next(1001);
+            mitArray[6] = rnd.Next(1001);
+            mitArray[7] = rnd.Next(1001);
+            mitArray[8] = rnd.Next(1001);
+            mitArray[9] = rnd.Next(1001);
+
+            int total = 0;
+            for (int i = 0; i < mitArray.Length; i++)
+            {
+                Console.WriteLine(total);
+                    total += mitArray[i];
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Summen er: " + total);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Opgave 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Opgave_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] forNavn = new string[7] { "Bjarne", "Quang", "Donald", "Joe", "Kim", "Peter", "Mark"};
+            string[] efterNavn = new string[7] { "Simonsen", "Jensen", "Trump", "Biden", "Jong-un", "Madsen", "Thomsen"};
+            Random rnd = new Random();
+
+            for ()
+            {
+                string randomFirstName = forNavn[rnd.Next(0, forNavn.Length)];
+                string randomLastName = efterNavn[rnd.Next(0, efterNavn.Length)];
+
+                Console.WriteLine(randomFirstName + " " + randomLastName);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
