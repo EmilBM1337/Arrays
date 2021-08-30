bb6586b96498e718e4eff3516f4125ea9a162af5
emil41m5 <emil41m5@CV-BB-5340>
2021-08-30 11:52:11 +0200

    .

--- Opgave 3/Program.cs
+++ Opgave 3/Program.cs
@@ -6,7 +6,36 @@ namespace Opgave_3
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Her er nogle tal.");
+            Console.WriteLine("");
+            int[] tal = new int[] { 0, 66, 666, 420, 1138, 1337, 7777};
+
+            for (int i = 0; i < tal.Length; i++)
+            {
+                Console.WriteLine(tal[i]);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Danmarks 5 største byer.");
+            string[] byNavne = new string[] { "Hovedstadsområdet", "Aarhus", "Odense", "Aalborg", "Esbjerg" };
+            Console.WriteLine("");
+
+            for (int u = 0; u < byNavne.Length; u++)
+            {
+                Console.WriteLine(byNavne[u]);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Elever i min klasse.");
+            string[] elever = new string[] { "Bjarne", "Bo", "Bob", "Brian", "Ben Kenobi" };
+            Console.WriteLine("");
+
+            for (int y = 0; y < elever.Length; y++)
+            {
+                Console.WriteLine(elever[y]);
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Opgave 4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Opgave_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
