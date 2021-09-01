758b2dbd0b2bc44bccf3d4cde0351a1a17135639
emil41m5 <emil41m5@CV-BB-5340>
2021-09-01 10:47:01 +0200

    .

--- Opgave 4/Program.cs
+++ Opgave 4/Program.cs
@@ -6,6 +6,8 @@ namespace Opgave_4
     {
         static void Main(string[] args)
         {
+            //Opgave 4.1
+            /*
             string[] navne = new string[5];
            
             Console.WriteLine("Indtast fem navne.");
@@ -19,6 +21,22 @@ namespace Opgave_4
             {
                 Console.WriteLine(navne [i]);
             }
+            */
+
+            //Opgave 4.2
+
+            int[] højde = new int[10];
+            int svar = 0;
+
+            Console.WriteLine("Indtast højden på ti personer, og jeg regner jeres gennemsit.");
+
+            for (int i = 0; i < 10; i++)
+            {
+                højde[i] = Convert.ToInt32(Console.ReadLine());
+                svar = svar + højde[i];
+            }
+            double x = svar / højde.Length;
+            Console.WriteLine("\nGennemsnittet af højden er: " + x);
         }
     }
 }
