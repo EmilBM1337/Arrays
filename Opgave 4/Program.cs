e317867cff247ba97e9825b30e174479b25a159a
emil41m5 <emil41m5@CV-BB-5340>
2021-09-01 09:31:14 +0200

    .

--- Opgave 4/Program.cs
+++ Opgave 4/Program.cs
@@ -6,7 +6,20 @@ namespace Opgave_4
     {
         static void Main(string[] args)
         {
-            
+            string[] navne = new string[5];
+           
+            Console.WriteLine("Indtast fem navne.");
+
+            for (int i = 0; i < 5; i++)
+            {
+                navne[i] = Console.ReadLine();
+            }
+            Console.WriteLine("");
+            Console.WriteLine("De fem naven du har indtastet er:");
+            for (int i = 0; i < navne.Length; i++)
+            {
+                Console.WriteLine(navne [i]);
+            }
         }
     }
 }
